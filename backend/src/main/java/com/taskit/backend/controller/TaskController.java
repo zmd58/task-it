dc77a24ee60df98fabac7256532dfa7990d41196
Jacqueline Rhea <j90rhea@gmail.com>
2023-03-28 19:15:19 -0400

    Added methods in TaskDAO, TaskJdbcDao, and Controller for get all tasks, create, and delete

--- backend/src/main/java/com/taskit/backend/controller/TaskController.java
+++ backend/src/main/java/com/taskit/backend/controller/TaskController.java
@@ -1,8 +1,37 @@
 package com.taskit.backend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.taskit.backend.dao.TaskDao;
+import com.taskit.backend.model.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/taskit/tasks")
 public class TaskController {
 
+    private TaskDao taskDao;
+
+    public TaskController(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+    @GetMapping()
+    public List<Task> getAllTasks() {
+        //need to add try/catch
+            return taskDao.findAllTasks();
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Task makeTask(@RequestBody Task task) {
+        //need to add try/catch
+        return taskDao.createTask(task);
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        //need to add try/catch
+        taskDao.deleteTask(id);
+    }
 }
